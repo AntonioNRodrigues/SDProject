fae1db49ef200d5721ec88fa06e86fb0c6157d00
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-10-18 16:07:20 +0100

    message passing 4 out of 6 tests.

--- src/message.c
+++ src/message.c
@@ -25,7 +25,7 @@ void free_message(struct message_t *msg) {
 }
 
 int message_to_buffer(struct message_t *msg, char **msg_buf) {
-
+	printf("BUFFER_TO_MESSAGE\n");
 	/* Verificar se msg é NULL */
 	if (msg != NULL) {
 
@@ -52,10 +52,11 @@ int message_to_buffer(struct message_t *msg, char **msg_buf) {
 			buffer_size += (_SHORT * 3 + _INT);
 
 		} else if (msg->c_type == CT_KEY) {
-			buffer_size += _SHORT * 3 + (strlen(msg->content.key) - 1);
+			buffer_size += _SHORT * 3 + (strlen(msg->content.key));
 
 		} else if (msg->c_type == CT_ENTRY) {
-			buffer_size += (_SHORT * 3 + _INT + msg->content.data->datasize);
+			buffer_size += (_SHORT * 3 + strlen(msg->content.entry->key) + _INT
+					+ msg->content.entry->value->datasize);
 		}
 		/* Alocar quantidade de memória determinada antes
 		 *msg_buf = ....
@@ -78,33 +79,33 @@ int message_to_buffer(struct message_t *msg, char **msg_buf) {
 		memcpy(ptr, &short_value, _SHORT);
 		ptr += _SHORT;
 
+		printf("C_TYPE:: %d\n", msg->c_type);
 		/* Consoante o conteúdo da mensagem, continuar a serialização da mesma */
 
 		switch (msg->c_type) {
 		case CT_ENTRY:
+
 			short_value = htons(strlen(msg->content.entry->key));
 			memcpy(ptr, &short_value, _SHORT);
 			ptr += _SHORT;
 
-			memcpy(ptr, &msg->content.entry->key,
-					strlen(msg->content.entry->key) - 1);
-			ptr += strlen(msg->content.keys) - 1;
+			int size = strlen(msg->content.entry->key);
+			memcpy(ptr, msg->content.entry->key, size);
+			ptr += size;
 
 			long_value = htonl(msg->content.entry->value->datasize);
 			memcpy(ptr, &long_value, _INT);
 			ptr += _INT;
 
-			memcpy(ptr, msg->content.entry->value->data,
-					msg->content.entry->value->datasize);
-			ptr += msg->content.entry->value->datasize;
+			int data_size = msg->content.entry->value->datasize;
+			memcpy(ptr, msg->content.entry->value->data, data_size);
+
 			break;
 		case CT_KEY:
-			short_value = htons(strlen(msg->content.key) - 1);
+			short_value = htons(strlen(msg->content.key));
 			memcpy(ptr, &short_value, _SHORT);
 			ptr += _SHORT;
-
-			memcpy(ptr, &msg->content.key, strlen(msg->content.key) - 1);
-			ptr += strlen(msg->content.key) - 1;
+			memcpy(ptr, msg->content.key, strlen(msg->content.key));
 			break;
 		case CT_KEYS:
 			long_value = htonl(nkeys);
@@ -125,9 +126,7 @@ int message_to_buffer(struct message_t *msg, char **msg_buf) {
 			long_value = htonl(msg->content.data->datasize);
 			memcpy(ptr, &long_value, _INT);
 			ptr += _INT;
-
-			memcpy(ptr, &msg->content.data->data, msg->content.data->datasize);
-			ptr += msg->content.data->datasize;
+			memcpy(ptr, msg->content.data->data, msg->content.data->datasize);
 			break;
 
 		case CT_RESULT:
@@ -144,7 +143,7 @@ int message_to_buffer(struct message_t *msg, char **msg_buf) {
 }
 
 struct message_t *buffer_to_message(char *msg_buf, int msg_size) {
-
+	printf("MESSAGE_TO_BUFFER");
 	if (msg_buf == NULL || msg_size <= 0)
 		return NULL;
 
@@ -161,7 +160,7 @@ struct message_t *buffer_to_message(char *msg_buf, int msg_size) {
 	memcpy(&short_aux, msg_buf, _SHORT);
 	msg->opcode = ntohs(short_aux);
 	msg_buf += _SHORT;
-
+	printf("OPCODE:: %d\n", msg->opcode);
 	memcpy(&short_aux, msg_buf, _SHORT);
 	msg->c_type = ntohs(short_aux);
 	msg_buf += _SHORT;
@@ -179,6 +178,8 @@ struct message_t *buffer_to_message(char *msg_buf, int msg_size) {
 	case CT_RESULT:
 		memcpy(&int_aux, msg_buf, _INT);
 		msg->content.result = ntohl(int_aux);
+		printf("C_TYPE:: %d\n", msg->c_type);
+		printf("RESULT %d\n", msg->content.result);
 		break;
 	case CT_KEY:
 
@@ -186,10 +187,12 @@ struct message_t *buffer_to_message(char *msg_buf, int msg_size) {
 		memcpy(&short_aux, msg_buf, _SHORT);
 		int size_key = ntohs(short_aux);
 		msg_buf += _SHORT;
+		printf("%d\n", size_key);
 		//KEY
 		msg->content.key = (char *) malloc(size_key + 1);
 		memcpy(msg->content.key, msg_buf, size_key);
 		msg->content.key[size_key] = '\0';
+		printf(msg->content.key);
 		break;
 	case CT_VALUE:
 		//DATASIZE
@@ -212,23 +215,36 @@ struct message_t *buffer_to_message(char *msg_buf, int msg_size) {
 		msg_buf += _SHORT;
 
 		//KEY
-		char *aux_key = (char *) malloc(size_key + 1);
-		memcpy(&aux_key, msg_buf, size_key);
-		aux_key[size_key] = '\0';
-		msg_buf += size_key;
+		char *aux_key = (char *) malloc(key_size + 1);
+		memcpy(aux_key, msg_buf, key_size);
+		aux_key[key_size] = '\0';
+		msg_buf += key_size;
 
 		//DATASIZE
 		memcpy(&int_aux, msg_buf, _INT);
 		data_size = ntohl(int_aux);
 		msg_buf += _INT;
-
-		//ENTRY
-		msg->content.entry = entry_create(aux_key, data_create(data_size));
-		if (msg->content.entry == NULL) {
-			free(msg);
+		printf("DATASIZE:::::%d\n",data_size);
+		struct data_t * temp_data = data_create(data_size);
+		if(temp_data == NULL){
+			free(aux_key);
+			free_message(msg);
 			return NULL;
 		}
-		memcpy(msg->content.entry, msg_buf, size_key + data_size);
+		//ENTRY
+
+		msg->content.entry = entry_create(aux_key, temp_data);
+		printf("---------------->%s:::: %d\n", msg->content.entry->key, msg->content.entry->value->datasize);
+		if (msg->content.entry == NULL) {
+			free(aux_key);
+			data_destroy(temp_data);
+			free_message(msg);
+			return NULL;
+		}
+		memcpy(msg->content.entry->value->data, msg_buf, data_size);
+		printf(msg->content.entry->key);
+		free(aux_key);
+		data_destroy(temp_data);
 		break;
 
 	case CT_KEYS:
@@ -248,7 +264,7 @@ struct message_t *buffer_to_message(char *msg_buf, int msg_size) {
 			msg_buf += size_key;
 			i++;
 		}
-		msg->content.keys[i]=NULL;
+		msg->content.keys[i] = NULL;
 		break;
 
 	default:
