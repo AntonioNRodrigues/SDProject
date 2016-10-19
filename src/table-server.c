73d54bfa275a302c108eb4365ce8025feb908e73
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-10-19 23:39:44 +0100

    process_message case OC_SIZE done.

--- src/table-server.c
+++ src/table-server.c
@@ -1,55 +1,85 @@
 /*
-   Programa que implementa um servidor de uma tabela hash com chainning.
-   Uso: table-server <porta TCP> <dimensão da tabela>
-   Exemplo de uso: ./table_server 54321 10
-*/
+ Programa que implementa um servidor de uma tabela hash com chainning.
+ Uso: table-server <porta TCP> <dimensão da tabela>
+ Exemplo de uso: ./table_server 54321 10
+ */
 #include <error.h>
 
 #include "inet.h"
 #include "table-private.h"
 #include "message-private.h"
 
-
 /* Função para preparar uma socket de receção de pedidos de ligação.
-*/
-int make_server_socket(short port){
-  int socket_fd;
-  struct sockaddr_in server;
+ */
+int make_server_socket(short port) {
+	int socket_fd;
+	struct sockaddr_in server;
 
-  if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
-    perror("Erro ao criar socket");
-    return -1;
-  }
+	if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("Erro ao criar socket");
+		return -1;
+	}
 
-  server.sin_family = AF_INET;
-  server.sin_port = htons(port);  
-  server.sin_addr.s_addr = htonl(INADDR_ANY);
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
 
-  if (bind(socket_fd, (struct sockaddr *) &server, sizeof(server)) < 0){
-      perror("Erro ao fazer bind");
-      close(socket_fd);
-      return -1;
-  }
+	if (bind(socket_fd, (struct sockaddr *) &server, sizeof(server)) < 0) {
+		perror("Erro ao fazer bind");
+		close(socket_fd);
+		return -1;
+	}
 
-  if (listen(socket_fd, 0) < 0){
-      perror("Erro ao executar listen");
-      close(sfd);
-      return -1;
-  }
-  return socket_fd;
+	if (listen(socket_fd, 0) < 0) {
+		perror("Erro ao executar listen");
+		close(sfd);
+		return -1;
+	}
+	return socket_fd;
 }
 
-
 /* Função que recebe uma tabela e uma mensagem de pedido e:
-	- aplica a operação na mensagem de pedido na tabela;
-	- devolve uma mensagem de resposta com oresultado.
-*/
-struct message_t *process_message(struct message_t *msg_pedido, struct table_t *tabela){
-	struct message_t *msg_resposta;
-	
+ - aplica a operação na mensagem de pedido na tabela;
+ - devolve uma mensagem de resposta com o resultado.
+ */
+struct message_t *process_message(struct message_t *msg_pedido,
+		struct table_t *tabela) {
+
+	struct message_t *msg_resposta = (struct message_t *) malloc(
+			sizeof(struct message_t));
+
+	int result;
+
 	/* Verificar parâmetros de entrada */
+	if (msg_pedido == NULL || tabela == NULL)
+		return NULL;
 
 	/* Verificar opcode e c_type na mensagem de pedido */
+
+	switch (msg_pedido->opcode) {
+	case OC_SIZE:
+		result = table_size(tabela);
+		//the table is empty
+		if (result < 0) {
+			msg_resposta->c_type = CT_RESULT;
+			msg_resposta->opcode = OC_RT_ERROR;
+			msg_resposta->content.result = result;
+		}
+		msg_resposta->c_type = CT_RESULT;
+		msg_resposta->opcode = OC_SIZE + 1;
+		msg_resposta->content.result = result;
+		break;
+	case OC_PUT:
+		break;
+	case OC_GET:
+		break;
+	case OC_UPDATE:
+		break;
+	case OC_DEL:
+		break;
+	default:
+		break;
+	}
 
 	/* Aplicar operação na tabela */
 
@@ -58,31 +88,30 @@ struct message_t *process_message(struct message_t *msg_pedido, struct table_t *
 	return msg_resposta;
 }
 
-
 /* Função "inversa" da função network_send_receive usada no table-client.
-   Neste caso a função implementa um ciclo receive/send:
+ Neste caso a função implementa um ciclo receive/send:
 
-	Recebe um pedido;
-	Aplica o pedido na tabela;
-	Envia a resposta.
-*/
-int network_receive_send(int sockfd, struct table_t *table){
-  char *message_resposta, *message_pedido;
-  int msg_length;
-  int message_size, msg_size, result;
-  struct message_t *msg_pedido, *msg_resposta;
-  struct list_t *results;
+ Recebe um pedido;
+ Aplica o pedido na tabela;
+ Envia a resposta.
+ */
+int network_receive_send(int sockfd, struct table_t *table) {
+	char *message_resposta, *message_pedido;
+	int msg_length;
+	int message_size, msg_size, result;
+	struct message_t *msg_pedido, *msg_resposta;
+	struct list_t *results;
 
 	/* Verificar parâmetros de entrada */
 
 	/* Com a função read_all, receber num inteiro o tamanho da 
-	   mensagem de pedido que será recebida de seguida.*/
+	 mensagem de pedido que será recebida de seguida.*/
 	result = read_all(sockfd, (char *) &msg_size, _INT);
 
 	/* Verificar se a receção teve sucesso */
 
 	/* Alocar memória para receber o número de bytes da
-	   mensagem de pedido. */
+	 mensagem de pedido. */
 
 	/* Com a função read_all, receber a mensagem de resposta. */
 	result = read_all(sockfd, message_pedido, /* tamanho da mensagem */);
@@ -103,10 +132,10 @@ int network_receive_send(int sockfd, struct table_t *table){
 	/* Verificar se a serialização teve sucesso */
 
 	/* Enviar ao cliente o tamanho da mensagem que será enviada
-	   logo de seguida
-	*/
+	 logo de seguida
+	 */
 	msg_size = htonl(message_size);
- 	result = write_all(server->/*atributo*/, (char *) &msg_size, _INT));
+	result = write_all(server->/*atributo*/, (char *) &msg_size, _INT));
 
 	/* Verificar se o envio teve sucesso */
 
@@ -121,38 +150,39 @@ int network_receive_send(int sockfd, struct table_t *table){
 	return 0;
 }
 
-
-
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
 	int listening_socket, connsock, result;
 	struct sockaddr_in client;
 	socklen_t size_client;
 	struct table_t *table;
 
-	if (argc != 2){
-	printf("Uso: ./server <porta TCP> <dimensão da tabela>\n");
-	printf("Exemplo de uso: ./table-server 54321 10\n");
-	return -1;
+	if (argc != 2) {
+		printf("Uso: ./server <porta TCP> <dimensão da tabela>\n");
+		printf("Exemplo de uso: ./table-server 54321 10\n");
+		return -1;
 	}
 
-	if ((listening_socket = make_server(atoi(argv[1]))) < 0) return -1;
+	if ((listening_socket = make_server(atoi(argv[1]))) < 0)
+		return -1;
 
-	if ((table = table_create(atoi(argv[2]))) == NULL){
-	result = close(listening_socket);
-	return -1;
+	if ((table = table_create(atoi(argv[2]))) == NULL) {
+		result = close(listening_socket);
+		return -1;
 	}
 
-	while ((connsock = accept(listening_socket, (struct sockaddr *) &client, &size_client)) != -1) {
+	while ((connsock = accept(listening_socket, (struct sockaddr *) &client,
+			&size_client)) != -1) {
 		printf(" * Client is connected!\n");
 
-		while (/* condição */){
+		while (/* condição */) {
 
 			/* Fazer ciclo de pedido e resposta */
-			network_receive_send(connsock, table) < 0);
+			network_receive_send(connsock, table) < 0
+		);
 
-			/* Ciclo feito com sucesso ? Houve erro?
-			   Cliente desligou? */
+		/* Ciclo feito com sucesso ? Houve erro?
+		 Cliente desligou? */
 
-		}
 	}
+}
 }
