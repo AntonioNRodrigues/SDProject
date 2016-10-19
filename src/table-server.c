8c4de85386805f075ce8efeeb9f0502d2df9902c
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-10-19 22:12:48 +0100

    new version of table-server.c

--- src/table-server.c
+++ src/table-server.c
@@ -1,14 +1,22 @@
-#include <errno.h>
+/*
+   Programa que implementa um servidor de uma tabela hash com chainning.
+   Uso: table-server <porta TCP> <dimensão da tabela>
+   Exemplo de uso: ./table_server 54321 10
+*/
+#include <error.h>
 
 #include "inet.h"
 #include "table-private.h"
 #include "message-private.h"
 
-int make_server(short port){
-  int sfd;
+
+/* Função para preparar uma socket de receção de pedidos de ligação.
+*/
+int make_server_socket(short port){
+  int socket_fd;
   struct sockaddr_in server;
 
-  if ((sfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+  if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
     perror("Erro ao criar socket");
     return -1;
   }
@@ -17,196 +25,134 @@ int make_server(short port){
   server.sin_port = htons(port);  
   server.sin_addr.s_addr = htonl(INADDR_ANY);
 
-  if (bind(sfd, (struct sockaddr *) &server, sizeof(server)) < 0){
+  if (bind(socket_fd, (struct sockaddr *) &server, sizeof(server)) < 0){
       perror("Erro ao fazer bind");
-      close(sfd);
+      close(socket_fd);
       return -1;
   }
 
-  if (listen(sfd, 0) < 0){
+  if (listen(socket_fd, 0) < 0){
       perror("Erro ao executar listen");
       close(sfd);
       return -1;
   }
-  return sfd;
+  return socket_fd;
 }
 
-struct message_t *get_msg_ok(int opcode_in, int res){
-  struct message_t *t = (struct message_t *) malloc(sizeof(struct message_t));
 
-  if (t == NULL) return NULL;
+/* Função que recebe uma tabela e uma mensagem de pedido e:
+	- aplica a operação na mensagem de pedido na tabela;
+	- devolve uma mensagem de resposta com oresultado.
+*/
+struct message_t *process_message(struct message_t *msg_pedido, struct table_t *tabela){
+	struct message_t *msg_resposta;
+	
+	/* Verificar parâmetros de entrada */
 
-  t->opcode = opcode_in + 1;
-  t->c_type = CT_RESULT;
-  t->content.result = res;
-  return t;
+	/* Verificar opcode e c_type na mensagem de pedido */
+
+	/* Aplicar operação na tabela */
+
+	/* Preparar mensagem de resposta */
+
+	return msg_resposta;
 }
 
-struct message_t *get_msg_tuple(int opcode_in, struct tuple_t *tp){
-  struct message_t *t = (struct message_t *) malloc(sizeof(struct message_t));
 
-  if (t == NULL) return NULL;
+/* Função "inversa" da função network_send_receive usada no table-client.
+   Neste caso a função implementa um ciclo receive/send:
 
-  t->opcode = opcode_in + 1;
-  t->c_type = CT_TUPLE;
-  t->content.tuple = tp;
-  return t;
-}
-
-int process_message(struct message_t *msg_in, struct table_t *tab, struct message_t **msg_out, struct list_t **results){
-  int res;
-  struct list_t *lres;
-
-  printf("OPCODE %d\n", msg_in->opcode);
-  switch (msg_in->opcode){
-    case OC_OUT:
-      res = table_put(tab, msg_in->content.tuple);
-      if (res == 0){
-        *msg_out = get_msg_ok(msg_in->opcode, 0);
-        *results = NULL;
-      } else
-        return res;
-      break;
-    case OC_IN:
-      lres = table_get(tab, msg_in->content.tuple, 0, 1);
-      if (lres != NULL){
-        if (list_size(lres) == 0){
-          *msg_out = get_msg_ok(msg_in->opcode, 0);
-          *results = NULL;
-        }
-        else{
-          *msg_out = get_msg_tuple(msg_in->opcode, lres->head->content->value);
-          *results = NULL;
-        }
-        list_destroy(lres);
-        free(msg_in);
-      } 
-      else
-        return -1;
-      break;
-    case OC_SIZE:
-      res = table_size(tab);
-      if (res >= 0){
-        *msg_out = get_msg_ok(msg_in->opcode, res);
-        *results = NULL;
-        printf("Size is %d\n", (*msg_out)->content.result);
-      } 
-      else
-        return res;
-      break;
-  }
-  return 0;
-}
-
-int network_receive_send(int sockfd, struct table_t *tab){
-  char *message_out, *message_in;
+	Recebe um pedido;
+	Aplica o pedido na tabela;
+	Envia a resposta.
+*/
+int network_receive_send(int sockfd, struct table_t *table){
+  char *message_resposta, *message_pedido;
   int msg_length;
-  int message_size, msg_size, res, n_tuples, i;
-  struct message_t *msg_in, *msg_out;
+  int message_size, msg_size, result;
+  struct message_t *msg_pedido, *msg_resposta;
   struct list_t *results;
 
-  if ((res = read_all(sockfd, (char *) &msg_length, _INT) <= 0)){
-    if (errno == EBADF)
-      printf(" * EBADF\n");
-    if (errno == EPIPE)
-      printf(" * EPIPE\n");
-    perror("Error while reading all"); 
-    return -1;
-  }
+	/* Verificar parâmetros de entrada */
 
-  msg_size = ntohl(msg_length);
-  printf("MSIZE %d\n", msg_size);
+	/* Com a função read_all, receber num inteiro o tamanho da 
+	   mensagem de pedido que será recebida de seguida.*/
+	result = read_all(sockfd, (char *) &msg_size, _INT);
 
-  if ((message_in = (char *) malloc(msg_size)) == NULL){
-    printf("Erro ao alocar mem para receber msg\n");
-    return -1; 
-  }
+	/* Verificar se a receção teve sucesso */
 
-  if ((res = read_all(sockfd, message_in, msg_size) < 0)){
-    free(message_in);
-    if (errno == EBADF)
-      printf(" * EBADF\n");
-    if (errno == EPIPE)
-      printf(" * EPIPE\n");
-    perror("Error while reading all"); 
-    return -1;
-    return -1;
-  }
+	/* Alocar memória para receber o número de bytes da
+	   mensagem de pedido. */
 
-  if ((msg_in = buffer_to_message(message_in, msg_size)) == NULL){
-    free(message_in);
-    printf("Erro ao desserializar a mensagem\n");
-    return -1;
-  }
+	/* Com a função read_all, receber a mensagem de resposta. */
+	result = read_all(sockfd, message_pedido, /* tamanho da mensagem */);
 
-  if ((res = process_message(msg_in, tab, &msg_out, &results)) < 0){
-    free_message(msg_in);
-    free(message_in);
-    printf("An error ocurred while processing a message.\n");
-    return res;
-  }
+	/* Verificar se a receção teve sucesso */
 
-  free_message(msg_in);
-  free(message_in);
+	/* Desserializar a mensagem do pedido */
+	msg_pedido = buffer_to_message(message_pedido, /* tamanho da mensagem */);
 
-  if ((message_size = message_to_buffer(msg_out, &message_out)) < 0){
-    printf("Erro ao serializar menssagem\n");
-    return message_size;
-  }
+	/* Verificar se a desserialização teve sucesso */
 
-  msg_size = htonl(message_size);
-  if ((res = write_all(sockfd, (char *) &msg_size, _INT)) < 0){
-    free_message(msg_out);
-    free(message_out);
-    return res;
-  }
-  if ((res = write_all(sockfd, message_out, message_size)) < 0){
-    free_message(msg_out);
-    free(message_out);
-    return res;
-  }
+	/* Processar a mensagem */
+	msg_resposta = process_message(msg_pedido, table);
 
-  free_message(msg_out);
-  free(message_out);
+	/* Serializar a mensagem recebida */
+	message_size = message_to_buffer(msg_resposta, &message_resposta);
 
-  if (results != NULL){
-    n_tuples = list_size(results);
-    
-    for (i = 0; i < n_tuples; i++){
-      
-    }
-  }
+	/* Verificar se a serialização teve sucesso */
 
-  return 0;
+	/* Enviar ao cliente o tamanho da mensagem que será enviada
+	   logo de seguida
+	*/
+	msg_size = htonl(message_size);
+ 	result = write_all(server->/*atributo*/, (char *) &msg_size, _INT));
+
+	/* Verificar se o envio teve sucesso */
+
+	/* Enviar a mensagem que foi previamente serializada */
+
+	result = write_all(server->/*atributo*/, message_resposta, message_size));
+
+	/* Verificar se o envio teve sucesso */
+
+	/* Libertar memória */
+
+	return 0;
 }
 
+
+
 int main(int argc, char **argv){
-  int wsock, connsock, res, client_on = 1;
-  struct sockaddr_in client;
-  socklen_t size_client;
-  struct table_t *table;
+	int listening_socket, connsock, result;
+	struct sockaddr_in client;
+	socklen_t size_client;
+	struct table_t *table;
 
-  if (argc != 2){
-    printf("Uso: ./server <porto_servidor>\n");
-    printf("Exemplo de uso: ./server 12345\n");
-    return -1;
-  }
+	if (argc != 2){
+	printf("Uso: ./server <porta TCP> <dimensão da tabela>\n");
+	printf("Exemplo de uso: ./table-server 54321 10\n");
+	return -1;
+	}
 
-  if ((wsock = make_server(atoi(argv[1]))) < 0) return -1;
+	if ((listening_socket = make_server(atoi(argv[1]))) < 0) return -1;
 
-  if ((table = table_create(100)) == NULL){
-    res = close(wsock);
-    return res;
-  }
+	if ((table = table_create(atoi(argv[2]))) == NULL){
+	result = close(listening_socket);
+	return -1;
+	}
 
-  while ((connsock = accept(wsock, (struct sockaddr *) &client, &size_client)) != -1) {
-    printf(" * Client is connected!\n");
-    while (client_on){
-      if (network_receive_send(connsock, table) < 0){
-        printf("maybe Enviar opcode erro\n");
-        client_on = 0;
-        close(connsock);
-      }
-    }
-  }
+	while ((connsock = accept(listening_socket, (struct sockaddr *) &client, &size_client)) != -1) {
+		printf(" * Client is connected!\n");
+
+		while (/* condição */){
+
+			/* Fazer ciclo de pedido e resposta */
+			network_receive_send(connsock, table) < 0);
+
+			/* Ciclo feito com sucesso ? Houve erro?
+			   Cliente desligou? */
+
+		}
+	}
 }
