13cb677f5a1005ec48c0414530414c39aaf24e8f
MiguelVale <miguel.brilha@gmail.com>
2016-10-28 13:02:31 +0100

    table-client prints messages

--- src/message.c
+++ src/message.c
@@ -292,3 +292,31 @@ int valid(short opcode, short c_type) {
 
 }
 
+void print_msg(struct message_t *msg) {
+	int i;
+	
+	printf("----- MESSAGE -----\n");
+	printf("opcode: %d, c_type: %d\n", msg->opcode, msg->c_type);
+	switch(msg->c_type) {
+		case CT_ENTRY:{
+			printf("key: %s\n", msg->content.entry->key);
+			printf("datasize: %d\n", msg->content.entry->value->datasize);
+		}break;
+		case CT_KEY:{
+			printf("key: %s\n", msg->content.key);
+		}break;
+		case CT_KEYS:{
+			for(i = 0; msg->content.keys[i] != NULL; i++) {
+				printf("key[%d]: %s\n", i, msg->content.keys[i]);
+			}
+		}break;
+		case CT_VALUE:{
+			printf("datasize: %d\n", msg->content.data->datasize);
+		}break;
+		case CT_RESULT:{
+			printf("result: %d\n", msg->content.result);
+		};
+	}
+	printf("-------------------\n");
+}
+
