ae28a877f700f60af5b78b6181cb7895115ec395
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-11-02 22:43:27 +0000

    defining the struct rtable_t.

---
+++ inc/client_stub-private.h
@@ -0,0 +1,13 @@
+#ifndef _CLIENT_STUB_PRIVATE_H
+#define _CLIENT_STUB_PRIVATE_H
+
+#include "data.h"
+
+/* Remote table. A definir pelo grupo em client_stub-private.h 
+ */
+struct rtable_t{
+	struct server_t *server;
+};
+
+
+#endif
--- src/client_stub.c
+++ src/client_stub.c
@@ -1,38 +1,36 @@
-/* Remote table. A definir pelo grupo em client_stub-private.h 
- */
-struct rtable_t;
+#include "client_stub-private.h"
 
-struct rtable_t *rtable_bind(const char *address_port){
+struct rtable_t *rtable_bind(const char *address_port) {
 
 }
 
-int rtable_unbind(struct rtable_t *rtable){
+int rtable_unbind(struct rtable_t *rtable) {
 
 }
 
-int rtable_put(struct rtable_t *rtable, char *key, struct data_t *value){
+int rtable_put(struct rtable_t *rtable, char *key, struct data_t *value) {
 
 }
 
-int rable_update(struct rtable_t *rtable, char *key, struct data_t *value){
+int rable_update(struct rtable_t *rtable, char *key, struct data_t *value) {
 
 }
 
-struct data_t *rtable_get(struct rtable_t *table, char *key){
+struct data_t *rtable_get(struct rtable_t *table, char *key) {
 
 }
-int rtable_del(struct rtable_t *table, char *key){
-
-}
-
-int rtable_size(struct rtable_t *rtable){
+int rtable_del(struct rtable_t *table, char *key) {
 
 }
 
-char **rtable_get_keys(struct rtable_t *rtable){
+int rtable_size(struct rtable_t *rtable) {
 
 }
 
-void rtable_free_keys(char **keys){
+char **rtable_get_keys(struct rtable_t *rtable) {
+
+}
+
+void rtable_free_keys(char **keys) {
 
 }
