1b8d9450b4f1e23cc1b0778b5077730f429ad990
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-11-03 13:54:31 +0000

    function rtable_bind done.

--- src/client_stub.c
+++ src/client_stub.c
@@ -1,6 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
 #include "client_stub-private.h"
 
 struct rtable_t *rtable_bind(const char *address_port) {
+	if (address_port == NULL) {
+		return NULL;
+	}
+	struct rtable_t *remote_table = (struct rtable_t*) malloc(
+			sizeof(struct rtable_t));
+	if (remote_table == NULL) {
+		return NULL;
+	}
+	remote_table->server = network_connect(address_port);
+	if (remote_table->server == NULL) {
+		free(remote_table);
+		return NULL;
+	}
+
+	return remote_table;
 
 }
 
