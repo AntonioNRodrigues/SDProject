2df432ec66cf5a05b28d2275122d3858fa1084a2
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-11-04 12:55:20 +0000

    put and update to do.

--- src/client_stub.c
+++ src/client_stub.c
@@ -7,7 +7,7 @@ struct rtable_t *rtable_bind(const char *address_port) {
 	if (address_port == NULL) {
 		return NULL;
 	}
-	struct rtable_t *remote_table = (struct rtable_t*) malloc(
+	struct rtable_t *remote_table = (struct rtable_t *) malloc(
 			sizeof(struct rtable_t));
 	if (remote_table == NULL) {
 		return NULL;
@@ -17,9 +17,7 @@ struct rtable_t *rtable_bind(const char *address_port) {
 		free(remote_table);
 		return NULL;
 	}
-
 	return remote_table;
-
 }
 
 int rtable_unbind(struct rtable_t *rtable) {
@@ -28,10 +26,10 @@ int rtable_unbind(struct rtable_t *rtable) {
 	}
 	int result = network_close(rtable->server);
 	if (result == 0)
-		printf("The connection has been closed without errors");
+		printf("The connection has been closed without errors\n");
 
 	if (result == -1)
-		printf("The connection has been closed with errors");
+		printf("The connection has been closed with errors\n");
 
 	free(rtable);
 	return result;
@@ -43,20 +41,88 @@ int rtable_put(struct rtable_t *rtable, char *key, struct data_t *value) {
 		return -1;
 	}
 
+
 	return 0;
 }
 
-int rable_update(struct rtable_t *rtable, char *key, struct data_t *value) {
-
+int rtable_update(struct rtable_t *rtable, char *key, struct data_t *value) {
+	if (rtable == NULL || key == NULL || value == NULL) {
+		return -1;
+	}
+	return 0;
 }
 
-struct data_t *rtable_get(struct rtable_t *table, char *key) {
+struct data_t *rtable_get(struct rtable_t *rtable, char *key) {
+	if (rtable == NULL || key == NULL) {
+		return NULL;
+	}
+	struct message_t *msg_out = (struct message_t *) malloc(
+			sizeof(struct message_t));
+	msg_out->opcode = OC_GET;
+	msg_out->c_type = CT_KEY;
+	msg_out->content.key = strdup(key);
+
+	struct message_t * msg_resposta = network_send_receive(rtable->server,
+			msg_out);
+	printf("Mensagem Enviada\n\n");
+	print_msg(msg_out);
+	free_message(msg_out);
+	printf("Mensagem Recebida\n\n");
+	if (msg_resposta == NULL) {
+		printf("Nao houve resposta\n");
+		return NULL;
+	} else {
+	print_msg(msg_resposta);
+		if (msg_resposta->opcode == OC_RT_ERROR) {
+			printf("Chave nao exista ou outro erro\n");
+			return NULL;
+		}
+		if (msg_resposta->c_type == CT_VALUE) {
+			printf("Valor: %s\n\n", msg_resposta->content.data->data);
+		}
+		//HAS TO BE CHANGED-------------------------------->
+		//free_message(msg_resposta);
+	}
+	return msg_resposta->content.data->data;
 
 }
-int rtable_del(struct rtable_t *table, char *key) {
+int rtable_del(struct rtable_t *rtable, char *key) {
+	if (rtable == NULL || key == NULL) {
+		return -1;
+	}
+	int ret = -1;
+	struct message_t *msg_out = (struct message_t *) malloc(
+			sizeof(struct message_t));
+	if (msg_out == NULL) {
+		return -1;
+	}
+	msg_out->opcode = OC_DEL;
+	msg_out->c_type = CT_KEY;
+	msg_out->content.key = strdup(key);
 
+	struct message_t *msg_resposta = network_send_receive(rtable->server,
+			msg_out);
+	printf("Mensagem Enviada\n\n");
+	print_msg(msg_out);
+	free_message(msg_out);
+	printf("Mensagem Recebida\n\n");
+	if (msg_resposta == NULL) {
+		printf("Nao houve resposta\n");
+		ret = -1;
+	} else {
+		print_msg(msg_resposta);
+		if (msg_resposta->opcode == OC_RT_ERROR) {
+			printf("Chave nao existe ou outro erro\n");
+			ret = -1;
+		} else {
+			printf("Resultado: %d\n\n", msg_resposta->content.result);
+			ret = 0;
+		}
+		free_message(msg_resposta);
+
+	}
+	return ret;
 }
-
 int rtable_size(struct rtable_t *rtable) {
 	if (rtable == NULL) {
 		return -1;
@@ -94,9 +160,43 @@ int rtable_size(struct rtable_t *rtable) {
 }
 
 char **rtable_get_keys(struct rtable_t *rtable) {
+	if (rtable == NULL)
+		return NULL;
+	struct message_t *msg_out = (struct message_t *) malloc(
+			sizeof(struct message_t));
+	msg_out->opcode = OC_GET;
+	msg_out->c_type = CT_KEY;
+	msg_out->content.key = strdup("!");
+
+	struct message_t * msg_resposta = network_send_receive(rtable->server,
+			msg_out);
+	printf("Mensagem Enviada\n\n");
+	print_msg(msg_out);
+	free_message(msg_out);
+	printf("Mensagem Recebida\n\n");
+	if (msg_resposta == NULL) {
+		printf("Nao houve resposta\n");
+		return NULL;
+	} else {
+		print_msg(msg_resposta);
+		if (msg_resposta->opcode == OC_RT_ERROR) {
+			printf("Chave nao exista ou outro erro\n");
+			return NULL;
+		}
+		if (msg_resposta->c_type == CT_KEYS) {
+			int i = 0;
+			while (msg_resposta->content.keys[i] != NULL) {
+				printf("key: %s\n\n", msg_resposta->content.keys[i]);
+				i++;
+			}
+		}
+	}
+	return msg_resposta->content.keys;
 
 }
 
 void rtable_free_keys(char **keys) {
-
+	if(keys != NULL){
+		table_free_keys(keys);
+	}
 }
