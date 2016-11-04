29fb27bb3565a3b9fa3df6f0242f18b851e67dfe
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-11-04 14:46:50 +0000

    include a global variable of the table and finished the init and destroy table.

--- src/table_skel.c
+++ src/table_skel.c
@@ -1,11 +1,26 @@
-int table_skel_init(int n_lists){
+#include <stdlib.h>
+#include <string.h>
 
+#include "table_skel-private.h"
+
+struct table_t *tabela;
+
+int table_skel_init(int n_lists) {
+	if (n_lists <= 0) {
+		return -1;
+	}
+	tabela = table_create(n_lists);
+	if (tabela == NULL) {
+		return -1;
+	}
+	return 0;
+}
+int table_skel_destroy() {
+	table_destroy(tabela);
+
+	return tabela == NULL ? 0 : -1;
 }
 
-int table_skel_destroy()  {
-
-}
-
-struct message_t *invoke(struct message_t *msg_in){
-
+struct message_t *invoke(struct message_t *msg_in) {
+	return NULL;
 }
