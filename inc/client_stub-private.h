6f855a9540089af2767b14329f06af7d02ff5ecb
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-11-04 12:53:48 +0000

    includes.

--- inc/client_stub-private.h
+++ inc/client_stub-private.h
@@ -1,13 +1,12 @@
 #ifndef _CLIENT_STUB_PRIVATE_H
 #define _CLIENT_STUB_PRIVATE_H
 
-#include "data.h"
+#include "client_stub.h"
 
 /* Remote table. A definir pelo grupo em client_stub-private.h 
  */
-struct rtable_t{
+struct rtable_t {
 	struct server_t *server;
 };
-
 
 #endif
