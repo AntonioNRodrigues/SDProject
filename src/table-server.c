f378a79bfe4e677c43a57a33bd743d8bc492e666
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-11-08 23:58:26 +0000

    multiple changes.
    changed the parameters of network_receive_send function.
    deleted the process_message.
    completed the main cycle with table skel init and table skel destroy

--- src/table-server.c
+++ src/table-server.c
@@ -27,16 +27,9 @@
 #include "table-private.h"
 #include "message-private.h"
 #include "network_client-private.h"
+#include "table_skel-private.h"
 
-/**
- * function to build the error message returned by the sever
- */
-struct message_t * build_error_msg(struct message_t *msg_error) {
-	msg_error->c_type = CT_RESULT;
-	msg_error->opcode = OC_RT_ERROR;
-	msg_error->content.result = -1;
-	return msg_error;
-}
+
 /* Função para preparar uma socket de receção de pedidos de ligação.
  */
 int make_server_socket(short port) {
@@ -79,125 +72,6 @@ int make_server_socket(short port) {
 	return socket_fd;
 }
 
-/* Função que recebe uma tabela e uma mensagem de pedido e:
- - aplica a operação na mensagem de pedido na tabela;
- - devolve uma mensagem de resposta com o resultado.
- */
-struct message_t *process_message(struct message_t *msg_pedido,
-		struct table_t *tabela) {
-	char *temp_key;
-	struct message_t *msg_resposta = (struct message_t *) malloc(
-			sizeof(struct message_t));
-	if (msg_resposta == NULL) {
-		return NULL;
-	}
-	int result;
-
-	/* Verificar parâmetros de entrada */
-	if (msg_pedido == NULL || tabela == NULL) {
-		free_message(msg_resposta);
-		return NULL;
-	}
-	/* Verificar opcode e c_type na mensagem de pedido */
-	if (msg_pedido->opcode < 0 || msg_pedido->c_type < 0) {
-		free_message(msg_resposta);
-		return NULL;
-	}
-	/* Aplicar operação na tabela */
-
-	/* Preparar mensagem de resposta */
-	switch (msg_pedido->opcode) {
-	case OC_SIZE:
-		result = table_size(tabela);
-		//the table is empty
-		if (result < 0) {
-			msg_resposta = build_error_msg(msg_resposta);
-		} else {
-			msg_resposta->c_type = CT_RESULT;
-			msg_resposta->opcode = OC_SIZE + 1;
-			msg_resposta->content.result = result;
-		}
-		break;
-	case OC_PUT:
-		result = table_put(tabela, msg_pedido->content.entry->key,
-				msg_pedido->content.entry->value);
-		//table_put failed
-		if (result == -1) {
-			msg_resposta = build_error_msg(msg_resposta);
-
-		} else {
-			msg_resposta->c_type = CT_RESULT;
-			msg_resposta->opcode = OC_PUT + 1;
-			msg_resposta->content.result = result;
-		}
-		break;
-	case OC_GET:
-
-		temp_key = strdup(msg_pedido->content.key);
-
-		// temp_key is NULL
-		if (temp_key == NULL) {
-			msg_resposta = build_error_msg(msg_resposta);
-		}
-		//key is ! --> GET ALL KEYS
-		if (strcmp("!", temp_key) == 0) {
-			if (tabela->quantity_entry == 0) {
-				msg_resposta = build_error_msg(msg_resposta);
-			} else {
-				msg_resposta->c_type = CT_KEYS;
-				msg_resposta->opcode = OC_GET + 1;
-				msg_resposta->content.keys = table_get_keys(tabela);
-			}
-		} else {
-			struct data_t *temp_data = table_get(tabela, temp_key);
-			//the key is present
-			if (temp_data != NULL) {
-				msg_resposta->c_type = CT_VALUE;
-				msg_resposta->opcode = OC_GET + 1;
-				msg_resposta->content.data = data_dup(temp_data);
-				data_destroy(temp_data);
-				//key does not exist
-			} else {
-				msg_resposta = build_error_msg(msg_resposta);
-			}
-		}
-		break;
-	case OC_UPDATE:
-		result = table_update(tabela, msg_pedido->content.entry->key,
-				msg_pedido->content.entry->value);
-		//table_update failed
-		if (result == -1) {
-			msg_resposta = build_error_msg(msg_resposta);
-		} else {
-			msg_resposta->c_type = CT_RESULT;
-			msg_resposta->opcode = OC_UPDATE + 1;
-			msg_resposta->content.result = result;
-		}
-		break;
-
-	case OC_DEL:
-		result = table_del(tabela, msg_pedido->content.key);
-		//table_del failed
-		if (result == -1) {
-			msg_resposta->c_type = CT_RESULT;
-			msg_resposta->opcode = OC_RT_ERROR;
-			msg_resposta->content.result = result;
-		} else {
-			msg_resposta->opcode = OC_DEL + 1;
-			msg_resposta->c_type = CT_RESULT;
-			msg_resposta->content.result = result;
-		}
-		break;
-	default:
-		break;
-	}
-	printf("Mensagem Recebida\n\n");
-	print_msg(msg_pedido);
-	printf("Mensagem Enviada\n\n");
-	print_msg(msg_resposta);
-	//free_message(msg_pedido);
-	return msg_resposta;
-}
 
 /* Função "inversa" da função network_send_receive usada no table-client.
  Neste caso a função implementa um ciclo receive/send:
@@ -206,13 +80,13 @@ struct message_t *process_message(struct message_t *msg_pedido,
  Aplica o pedido na tabela;
  Envia a resposta.
  */
-int network_receive_send(int sockfd, struct table_t *table) {
+int network_receive_send(int sockfd) {
 	char *message_resposta, *message_pedido;
 	int message_size, msg_size, result;
 	struct message_t *msg_pedido, *msg_resposta;
 
 	/* Verificar parâmetros de entrada */
-	if (table == NULL || sockfd == -1) {
+	if (sockfd == -1) {
 		return -1;
 	}
 
@@ -248,7 +122,7 @@ int network_receive_send(int sockfd, struct table_t *table) {
 		return -1;
 	}
 	/* Processar a mensagem */
-	msg_resposta = process_message(msg_pedido, table);
+	msg_resposta = invoke(msg_pedido);
 	/* Serializar a mensagem recebida */
 	message_size = message_to_buffer(msg_resposta, &message_resposta);
 
@@ -305,7 +179,6 @@ int main(int argc, char **argv) {
 	struct pollfd connections[NFDS];
 	int listening_socket, result;
 	int number_clients = 1;
-	struct table_t *table;
 
 	//test argc
 	if (argc != 3) {
@@ -318,8 +191,8 @@ int main(int argc, char **argv) {
 	if ((listening_socket = make_server_socket(atoi(argv[1]))) < 0) {
 		return -1;
 	}
-	//init table --> to be changed to table_skell_init
-	if ((table = table_create(atoi(argv[2]))) == NULL) {
+	//table_skel_init
+	if (table_skel_init(atoi(argv[2])) == -1) {
 		printf("table\n");
 		result = close(listening_socket);
 		return -1;
@@ -349,7 +222,7 @@ int main(int argc, char **argv) {
 				int free_index = find_free_connection(connections);
 
 				//-1 there is no space in the array --> do not accept socket
-				if (free_index != -1 ) {
+				if (free_index != -1) {
 					if ((connections[free_index].fd = accept(connections[0].fd,
 							(struct sockaddr *) &client, &size_client)) > 0) {
 						connections[free_index].events = POLLIN;
@@ -365,16 +238,17 @@ int main(int argc, char **argv) {
 				//if socket has data to read
 				if (connections[j].revents & POLLIN) {
 					/*if (network_receive_send(connections[j].fd, table) < 0) {
-						printf("client quit");
-						close(connections[j].fd);
-						number_clients--;
-						connections[j].fd = -1;
-						printf("closed connection %d", number_clients);
-					} else {*/
-						network_receive_send(connections[j].fd, table);
+					 printf("client quit");
+					 close(connections[j].fd);
+					 number_clients--;
+					 connections[j].fd = -1;
+					 printf("closed connection %d", number_clients);
+					 } else {*/
+					network_receive_send(connections[j].fd);
 					/*}*/
 				}
-				if(connections[j].revents == POLLHUP || connections[j].revents == POLLERR){
+				if (connections[j].revents == POLLHUP
+						|| connections[j].revents == POLLERR) {
 					close(connections[j].fd);
 					connections[j].fd = -1;
 					number_clients--;
