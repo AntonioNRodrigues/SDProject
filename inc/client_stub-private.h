b6b23cbde242c43e456328b16527d0d5fde1acf1
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-11-15 12:22:26 +0000

    defined the value for the timeout and a new function retry()

--- inc/client_stub-private.h
+++ inc/client_stub-private.h
@@ -1,6 +1,9 @@
 #ifndef _CLIENT_STUB_PRIVATE_H
 #define _CLIENT_STUB_PRIVATE_H
 
+//the time out to retry sending the message if the server fails to responds.
+#define TIMEOUT_RETRY 5000
+
 #include "client_stub.h"
 
 /* Remote table. A definir pelo grupo em client_stub-private.h 
@@ -8,5 +11,9 @@
 struct rtable_t {
 	struct server_t *server;
 };
+/**
+ * function to define a retry after a TIMEOUT_RETRY of miliseconds
+ */
+void retry();
 
 #endif
