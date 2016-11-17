7d6637bcf033a0ca4f44394eade770c5c7244dae
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-11-17 18:00:57 +0000

    inserted the define that were in the table-server

--- inc/table_skel-private.h
+++ inc/table_skel-private.h
@@ -1,9 +1,13 @@
 #ifndef _TABLE_SKEL_PRIVATE_H
 #define _TABLE_SKEL_PRTIVATE_H
 
+#define MAX_SOCKETS 5
+#define TIMEOUT -1
+#define LISTENING_SOCKET_POS 0
+
 #include "table-private.h"
 #include "table_skel.h"
 
-struct message_t * build_error_msg(struct message_t *msg_error);
+struct message_t *build_error_msg(struct message_t *msg_error);
 
 #endif
