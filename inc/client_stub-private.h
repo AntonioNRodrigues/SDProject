2fa25171a5b7441221f097fec7c698973fa773a2
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-11-24 18:00:10 +0000

    redefined the struct rtable_t now has two servers.

--- inc/client_stub-private.h
+++ inc/client_stub-private.h
@@ -8,15 +8,19 @@
 #ifndef _CLIENT_STUB_PRIVATE_H
 #define _CLIENT_STUB_PRIVATE_H
 
-//the time out to retry sending the message if the server fails to responds.
+/*the time out to retry sending the message if the server fails to responds.*/
 #define RETRY_TIME 5000
+/*quantity of servers*/
+#define N_SERVERS 2
 
 #include "client_stub.h"
 
 /* Remote table. A definir pelo grupo em client_stub-private.h 
  */
 struct rtable_t {
-	struct server_t *server;
+	struct server_t *server_one;
+	struct server_t *server_two;
+
 };
 /**
  * function to define a retry after a RETRY_TIME of miliseconds
@@ -26,5 +30,11 @@ int retry(struct rtable_t *remote_table);
  * function to reestablish the connection
  */
 struct rtable_t *rtable_rebind(struct rtable_t *remote_table);
+
+/**
+ * function to prepare the backup server. Fill all the
+ * struct server_t server_two
+ */
+int prepare_backup_server(struct rtable_t * remote_table, const char *address_port);
 
 #endif
