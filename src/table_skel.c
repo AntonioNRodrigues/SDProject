f484a1998976152593898b5e3bc38dcbe50ab836
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-12-08 19:59:46 +0000

    way to process the OC_PUT+1 OC_UPGRADE+1 and OC_DEL+1 in thetable.

--- src/table_skel.c
+++ src/table_skel.c
@@ -32,7 +32,6 @@ int table_skel_destroy() {
 	return tabela == NULL ? 0 : -1;
 }
 
-
 struct message_t * build_error_msg(struct message_t *msg_error) {
 	msg_error->c_type = CT_RESULT;
 	msg_error->opcode = OC_RT_ERROR;
@@ -55,7 +54,7 @@ void print_status() {
 			i++;
 			free(temp_data);
 		}
-	}else{
+	} else {
 		printf("The table is empty\n");
 	}
 	table_free_keys(keys);
@@ -100,7 +99,7 @@ struct message_t *invoke(struct message_t *msg_in) {
 			msg_resposta->content.result = result;
 		}
 		break;
-	case OC_PUT:
+	case (OC_PUT):
 		result = table_put(tabela, msg_in->content.entry->key,
 				msg_in->content.entry->value);
 		//table_put failed
@@ -118,6 +117,25 @@ struct message_t *invoke(struct message_t *msg_in) {
 			msg_resposta->content.result = result;
 		}
 		break;
+	case (OC_PUT + 1):
+		result = table_put(tabela, msg_in->content.entry->key,
+				msg_in->content.entry->value);
+		//table_put failed
+		if (result == -1) {
+			msg_resposta = build_error_msg(msg_resposta);
+			//table_put of a key that is in the system
+		} else if (result == -10) {
+			msg_resposta->c_type = CT_RESULT;
+			msg_resposta->opcode = OC_PUT + 2;
+			msg_resposta->content.result = -10;
+			//table put success
+		} else {
+			msg_resposta->c_type = CT_RESULT;
+			msg_resposta->opcode = OC_PUT + 2;
+			msg_resposta->content.result = result;
+		}
+		break;
+
 	case OC_GET:
 
 		temp_key = strdup(msg_in->content.key);
@@ -169,8 +187,30 @@ struct message_t *invoke(struct message_t *msg_in) {
 			msg_resposta->content.result = result;
 		}
 		break;
-
+	case OC_UPDATE + 1:
+		result = table_update(tabela, msg_in->content.entry->key,
+				msg_in->content.entry->value);
+		//table_update failed
+		if (result == -1) {
+			msg_resposta = build_error_msg(msg_resposta);
+		} else {
+			msg_resposta->c_type = CT_RESULT;
+			msg_resposta->opcode = OC_UPDATE + 2;
+			msg_resposta->content.result = result;
+		}
+		break;
 	case OC_DEL:
+		result = table_del(tabela, msg_in->content.key);
+		//table_del failed
+		if (result == -1) {
+			msg_resposta = build_error_msg(msg_resposta);
+		} else {
+			msg_resposta->opcode = OC_DEL + 1;
+			msg_resposta->c_type = CT_RESULT;
+			msg_resposta->content.result = result;
+		}
+		break;
+	case OC_DEL + 1:
 		result = table_del(tabela, msg_in->content.key);
 		//table_del failed
 		if (result == -1) {
